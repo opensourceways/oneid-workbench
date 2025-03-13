91de7d9bae38166cfee4cde2f2fa0f88d08d524b
tfhddd <2272751277@qq.com>
2025-03-13 21:26:25 +0800

    [ADD]add frame

---
+++ src/main/java/com/oneid/common/constant/TableConstant.java
@@ -0,0 +1,29 @@
+/* Copyright (c) 2025 openEuler Community
+ oneid-workbench is licensed under the Mulan PSL v2.
+ You can use this software according to the terms and conditions of the Mulan PSL v2.
+ You may obtain a copy of Mulan PSL v2 at:
+     http://license.coscl.org.cn/MulanPSL2
+ THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ See the Mulan PSL v2 for more details.
+*/
+
+package com.oneid.common.constant;
+
+public class TableConstant {
+    /**
+     * table name of PersonalApiTokenDO.
+     */
+    public static final String PERSONAL_API_TOKEN_TABLE = "personal_api_token";
+
+    /**
+     * PersonalApiPermissionDO.
+     */
+    public static final String PERSONAL_API_PERMISSION_TABLE = "personal_api_permission";
+
+    /**
+     * PersonalApiUrlDO.
+     */
+    public static final String PERSONAL_API_URL_TABLE = "personal_api_url";
+}
--- src/main/java/com/oneid/controller/PersonalApiController.java
+++ src/main/java/com/oneid/controller/PersonalApiController.java
@@ -12,27 +12,12 @@
 package com.oneid.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * CommonAPIController 通用 API 控制器，处理通用的 API 端点和功能.
+ * 私人令牌.
  */
 @RequestMapping(value = "/oneid-workbench")
 @RestController
-public class CommonAPIController {
-    /**
-     * 表示服务正常状态的常量.
-     */
-    private static final String SERVICE_STATUS_NORMAL = "normal";
-
-    /**
-     * 检查 OM 服务的方法.
-     *
-     * @return SERVICE_STATUS_NORMAL
-     */
-    @RequestMapping(value = "/checkOmService", method = RequestMethod.GET)
-    public String checkOmService() {
-        return SERVICE_STATUS_NORMAL;
-    }
+public class PersonalApiController {
 }
